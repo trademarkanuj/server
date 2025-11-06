691c79064a5e5766e7f296867c218b54820960a0
anujsharma121 <anuj.sharma@unoapp.io>
2025-11-06 14:53:33 +0530

    vercel setup

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .index import index
+
+urlpatterns = [
+    path("", index),
+]
--- server/urls.py
+++ server/urls.py
@@ -21,6 +21,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chat.urls')),
+    path("api/", include("api.urls")),
 ]
