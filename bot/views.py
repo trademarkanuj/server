a0586c69f8c0b63b96170c1bcecf4b1d8b1aef0c
anujsharma121 <anuj.sharma@unoapp.io>
2025-11-07 22:05:56 +0530

    secret key

--- bot/views.py
+++ bot/views.py
@@ -3,13 +3,12 @@ from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.conf import settings
-
 def home(request):
     return render(request, "index.html")
 
 @csrf_exempt
 def chat_api(request):
-    required_key = getattr(settings, "CHATBOT_API_KEY", None)
+    required_key = getattr(settings, "g0L1ritGqF6LYpbcikIo9Lno4QARTo2rKs2UGJi0QD7l8I0Q23ja3eI2OhtYzS6vDW8", None)
     client_key = request.headers.get("X-API-KEY")
 
     if required_key and client_key != required_key:
